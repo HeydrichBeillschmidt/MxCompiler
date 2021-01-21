0b9693e7df52e454f3ec23e9cde7efe9d20038f5
Heydrich_B <56252029+HeydrichBeillschmidt@users.noreply.github.com>
2021-01-21 14:58:35 +0800

    Utilities for semantic check

--- src/Utils/Scope.java
+++ src/Utils/Scope.java
@@ -1,23 +1,45 @@
 package Mx.Utils;
 
+import Mx.AST.DeclarationNode;
+import Mx.AST.FuncNode;
+import Mx.AST.Specifiers.BuiltInTypeSpecifierNode;
+import Mx.AST.Specifiers.DeclSpecifierSeqNode;
+import Mx.AST.VarNode;
 import Mx.Entities.*;
+import Mx.Types.Type;
+import Mx.Utils.Errors.SemanticError;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Scope {
-    private Scope parentScope;
-    private ArrayList<Scope> childrenScope;
-    private Map<String, Entity> entities;
+    public enum ScopeType {
+        globalScope, classScope, funcScope, loopScope, blockScope
+    }
+    private final ScopeType scopeType;
 
-    public Scope(Scope parent) {
-        this.parentScope = parent;
+    private final Scope parentScope;
+    private final ArrayList<Scope> childrenScope;
+
+    private final Map<String, VarEntity> varEntities;
+    private final Map<String, FunctionEntity> funcEntities;
+    private final Set<FunctionEntity> constructorEntities;
+
+    private final DeclSpecifierSeqNode funcRetType;
+    private final Type classType;
+
+    public Scope(Scope parentScope, ScopeType scopeType,
+                 DeclSpecifierSeqNode funcRetType, Type classType) {
+        this.scopeType = scopeType;
+        this.parentScope = parentScope;
         if (this.parentScope != null) {
-            parentScope.childrenScope.add(this);
+            this.parentScope.childrenScope.add(this);
         }
         this.childrenScope = new ArrayList<>();
-        this.entities = new HashMap<>();
+        this.varEntities = new HashMap<>();
+        this.funcEntities = new HashMap<>();
+        this.constructorEntities = new HashSet<>();
+        this.funcRetType = funcRetType;
+        this.classType = classType;
     }
 
     public Scope getParentScope() {
@@ -26,7 +48,248 @@ public class Scope {
     public ArrayList<Scope> getChildrenScope() {
         return childrenScope;
     }
-    public Map<String, Entity> getEntities() {
-        return entities;
+    public Map<String, VarEntity> getVarEntities() {
+        return varEntities;
+    }
+    public Map<String, FunctionEntity> getFuncEntities() {
+        return funcEntities;
+    }
+    public Set<FunctionEntity> getConstructorEntities() {
+        return constructorEntities;
+    }
+    public ScopeType getScopeType() {
+        return scopeType;
+    }
+    public DeclSpecifierSeqNode getFuncRetType() {
+        return funcRetType;
+    }
+    public Type getClassType() {
+        return classType;
+    }
+
+    public void generateBuiltinFunctions() {
+        Location defaultLoc = new Location(0,0);
+        FunctionEntity builtinFunc;
+        ArrayList<VarEntity> builtinFuncParameters;
+
+        // void print(string str);
+        builtinFuncParameters = new ArrayList<>();
+        builtinFuncParameters.add(VarEntity.builtinMethodParameter("str", "string"));
+        builtinFunc = new FunctionEntity("print", defaultLoc,
+                new DeclSpecifierSeqNode(defaultLoc, new BuiltInTypeSpecifierNode(defaultLoc, "void")),
+                builtinFuncParameters, null, FunctionEntity.FuncEntityType.function);
+        funcEntities.put("print", builtinFunc);
+
+        // void println(string str);
+        builtinFuncParameters.clear();
+        builtinFuncParameters.add(VarEntity.builtinMethodParameter("str", "string"));
+        builtinFunc = new FunctionEntity("println", defaultLoc,
+                new DeclSpecifierSeqNode(defaultLoc, new BuiltInTypeSpecifierNode(defaultLoc, "void")),
+                builtinFuncParameters, null, FunctionEntity.FuncEntityType.function);
+        funcEntities.put("println", builtinFunc);
+
+        // void printInt(int n);
+        builtinFuncParameters.clear();
+        builtinFuncParameters.add(VarEntity.builtinMethodParameter("n", "int"));
+        builtinFunc = new FunctionEntity("printInt", defaultLoc,
+                new DeclSpecifierSeqNode(defaultLoc, new BuiltInTypeSpecifierNode(defaultLoc, "void")),
+                builtinFuncParameters, null, FunctionEntity.FuncEntityType.function);
+        funcEntities.put("printInt", builtinFunc);
+
+        // void printlnInt(int n);
+        builtinFuncParameters.clear();
+        builtinFuncParameters.add(VarEntity.builtinMethodParameter("n", "int"));
+        builtinFunc = new FunctionEntity("printlnInt", defaultLoc,
+                new DeclSpecifierSeqNode(defaultLoc, new BuiltInTypeSpecifierNode(defaultLoc, "void")),
+                builtinFuncParameters, null, FunctionEntity.FuncEntityType.function);
+        funcEntities.put("printlnInt", builtinFunc);
+
+        // string getString();
+        builtinFuncParameters.clear();
+        builtinFunc = new FunctionEntity("getString", defaultLoc,
+                new DeclSpecifierSeqNode(defaultLoc, new BuiltInTypeSpecifierNode(defaultLoc, "string")),
+                builtinFuncParameters, null, FunctionEntity.FuncEntityType.function);
+        funcEntities.put("getString", builtinFunc);
+
+        // int getInt();
+        builtinFuncParameters.clear();
+        builtinFunc = new FunctionEntity("getInt", defaultLoc,
+                new DeclSpecifierSeqNode(defaultLoc, new BuiltInTypeSpecifierNode(defaultLoc, "int")),
+                builtinFuncParameters, null, FunctionEntity.FuncEntityType.function);
+        funcEntities.put("getInt", builtinFunc);
+
+        // string toString(int i);
+        builtinFuncParameters.clear();
+        builtinFuncParameters.add(VarEntity.builtinMethodParameter("i", "int"));
+        builtinFunc = new FunctionEntity("toString", defaultLoc,
+                new DeclSpecifierSeqNode(defaultLoc, new BuiltInTypeSpecifierNode(defaultLoc, "string")),
+                builtinFuncParameters, null, FunctionEntity.FuncEntityType.function);
+        funcEntities.put("toString", builtinFunc);
+    }
+    public void declareVariable(DeclarationNode decl,
+                                VarEntity.VarEntityType varType,
+                                ExceptionHandler exceptionHandler) {
+        VarEntity entity = null;
+        if (decl instanceof VarNode) {
+            entity = ((VarNode) decl).generateEntity(varType);
+        }
+        assert entity != null;
+        if (varEntities.containsKey(entity.getName())) {
+            exceptionHandler.error("Duplicate declaration of variable \""
+                    + entity.getName() + "\".", decl.getLocation());
+            throw new SemanticError();
+        }
+        else {
+            varEntities.put(entity.getName(), entity);
+        }
+    }
+    public void declareVariable(VarEntity entity, Location loc,
+                                ExceptionHandler exceptionHandler) {
+        assert entity != null;
+        if (varEntities.containsKey(entity.getName())) {
+            exceptionHandler.error("Duplicate declaration of variable \""
+                    + entity.getName() + "\".", loc);
+            throw new SemanticError();
+        }
+        else {
+            varEntities.put(entity.getName(), entity);
+        }
+    }
+    public void declareFunction(DeclarationNode decl,
+                                FunctionEntity.FuncEntityType funcType,
+                                ExceptionHandler exceptionHandler) {
+        FunctionEntity entity = null;
+        if (decl instanceof FuncNode) {
+            entity = ((FuncNode) decl).generateEntity(funcType);
+        }
+        assert entity != null;
+        if (funcEntities.containsKey(entity.getName())) {
+            exceptionHandler.error("Duplicate declaration of function \""
+                    + entity.getName() + "\".", decl.getLocation());
+            throw new SemanticError();
+        }
+        else if (funcType==FunctionEntity.FuncEntityType.constructor) {
+            exceptionHandler.error("Mistake declaration of constructor as function",
+                    decl.getLocation());
+            throw new SemanticError();
+        }
+        else {
+            funcEntities.put(entity.getName(), entity);
+        }
+    }
+    public void declareFunction(FunctionEntity entity, Location loc,
+                                ExceptionHandler exceptionHandler) {
+        assert entity != null;
+        if (funcEntities.containsKey(entity.getName())) {
+            exceptionHandler.error("Duplicate declaration of function \""
+                    + entity.getName() + "\".", loc);
+            throw new SemanticError();
+        }
+        else if (entity.getFuncEntityType()==FunctionEntity.FuncEntityType.constructor) {
+            exceptionHandler.error("Mistake declaration of constructor as function", loc);
+            throw new SemanticError();
+        }
+        else {
+            funcEntities.put(entity.getName(), entity);
+        }
+    }
+    public void declareConstructor(DeclarationNode decl,
+                                   ExceptionHandler exceptionHandler) {
+        FunctionEntity entity = null;
+        if (decl instanceof FuncNode) {
+            entity = ((FuncNode) decl).generateEntity(FunctionEntity.FuncEntityType.constructor);
+        }
+        assert entity != null;
+        if (constructorEntities.contains(entity)) {
+            exceptionHandler.error("Duplicate declaration of constructor.", decl.getLocation());
+            throw new SemanticError();
+        }
+        else {
+            constructorEntities.add(entity);
+        }
+    }
+    public void declareConstructor(FunctionEntity entity, Location loc,
+                                   ExceptionHandler exceptionHandler) {
+        assert entity != null;
+        if (entity.getFuncEntityType()!=FunctionEntity.FuncEntityType.constructor) {
+            exceptionHandler.error("Mistake declaration of function as constructor", loc);
+            throw new SemanticError();
+        }
+        else if (constructorEntities.contains(entity)) {
+            exceptionHandler.error("Duplicate declaration of constructor.", loc);
+            throw new SemanticError();
+        }
+        else {
+            constructorEntities.add(entity);
+        }
+    }
+    public VarEntity getVarEntity(String name) {
+        if (varEntities.containsKey(name)) {
+            return varEntities.get(name);
+        }
+        else if (parentScope!=null) {
+            return parentScope.getVarEntity(name);
+        }
+        else return null;
+    }
+    public FunctionEntity getFuncEntity(String name) {
+        if (funcEntities.containsKey(name)) {
+            return funcEntities.get(name);
+        }
+        else if (parentScope!=null) {
+            return parentScope.getFuncEntity(name);
+        }
+        else return null;
+    }
+    public FunctionEntity getConstructorEntity(ArrayList<VarNode> parameters) {
+        for (var ce: constructorEntities) {
+            if (parameters.size()==ce.getParameters().size()) {
+                boolean fit = true;
+                for (int i = 0, it = parameters.size(); i < it; ++i) {
+                    VarNode par = parameters.get(i);
+                    VarEntity parEntity = ce.getParameters().get(i);
+                    if (!(par.getSpecifier().equals(parEntity.getSpecifier())) ||
+                            !(par.getName().equals(parEntity.getName()))) {
+                        fit = false;
+                        break;
+                    }
+                }
+                if (fit) return ce;
+            }
+        }
+        return null;
+    }
+    public boolean inGlobalScope() {
+        return parentScope==null;
+    }
+    public boolean inClassScope() {
+        if (scopeType==ScopeType.classScope) {
+            return true;
+        }
+        else if (scopeType==ScopeType.globalScope) {
+            return false;
+        }
+        else return parentScope.inClassScope();
+    }
+    public boolean inFuncScope() {
+        if (scopeType==ScopeType.funcScope) {
+            return true;
+        }
+        else if (scopeType==ScopeType.globalScope) {
+            return false;
+        }
+        else return parentScope.inFuncScope();
+    }
+    public boolean inLoopScope() {
+        if (scopeType==ScopeType.loopScope) {
+            return true;
+        }
+        else if (scopeType==ScopeType.globalScope) {
+            return false;
+        }
+        else return parentScope.inLoopScope();
+    }
+    public boolean inMethodScope() {
+        return inClassScope() && inFuncScope();
     }
 }
---
+++ src/Utils/TypeTable.java
@@ -0,0 +1,106 @@
+package Mx.Utils;
+
+import Mx.AST.Specifiers.*;
+import Mx.Types.*;
+import Mx.Utils.Errors.SemanticError;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TypeTable {
+    private final Map<String, Type> typeTable;
+
+    public TypeTable() {
+        typeTable = new HashMap<>();
+
+        typeTable.put("int"     , new IntType());
+        typeTable.put("bool"    , new BoolType());
+        typeTable.put("string"  , new StringType());
+        typeTable.put("void"    , new VoidType());
+    }
+
+    public Map<String, Type> getTypeTable() {
+        return typeTable;
+    }
+    public boolean hasType(DeclSpecifierSeqNode specifier) {
+        if (specifier==null) return false;
+        return typeTable.containsKey(specifier.getTypename());
+    }
+    public boolean hasType(String typeName) {
+        if (typeName==null) return false;
+        return typeTable.containsKey(typeName);
+    }
+    public void defType(DeclSpecifierSeqNode specifier, Type type,
+                    ExceptionHandler exceptionHandler) throws SemanticError {
+        if (hasType(specifier)) {
+            exceptionHandler.error("Duplicate definition of type \""
+                + specifier.getTypename() + "\".");
+            throw new SemanticError();
+        }
+        else
+            typeTable.put(specifier.getTypename(), type);
+    }
+    public Type getType(DeclSpecifierSeqNode specifier,
+                        ExceptionHandler exceptionHandler) throws SemanticError {
+        if (specifier==null) {
+            throw new SemanticError();
+        }
+        if (!hasType(specifier)) {
+            exceptionHandler.error("Undefined type \""
+                    + specifier.getTypename() + "\".");
+            throw new SemanticError();
+        }
+        DeclSpecifierNode typeSpecifier = specifier.getTypeSpecifier();
+        if (typeSpecifier instanceof ArrayTypeSpecifierNode) {
+            String baseTypeName
+                    = ((ArrayTypeSpecifierNode) typeSpecifier).getBaseTypeSpecifier().getTypename();
+            int dimension = ((ArrayTypeSpecifierNode) typeSpecifier).getDimension();
+            return new ArrayType(typeTable.get(baseTypeName), dimension);
+        }
+        else
+            return typeTable.get(specifier.getTypename());
+    }
+    public Type getType(DeclSpecifierSeqNode specifier) {
+        DeclSpecifierNode typeSpecifier = specifier.getTypeSpecifier();
+        if (typeSpecifier instanceof ArrayTypeSpecifierNode) {
+            String baseTypeName
+                    = ((ArrayTypeSpecifierNode) typeSpecifier).getBaseTypeSpecifier().getTypename();
+            int dimension = ((ArrayTypeSpecifierNode) typeSpecifier).getDimension();
+            return new ArrayType(typeTable.get(baseTypeName), dimension);
+        }
+        else
+            return typeTable.get(specifier.getTypename());
+    }
+    public Type getType(String typeName) {
+        return typeTable.get(typeName);
+    }
+
+    public void checkTypes(ExceptionHandler h) {
+        for (var type: typeTable.values())
+            checkRecursiveDefinition(type, h);
+    }
+    private void checkRecursiveDefinition(Type t, ExceptionHandler h) {
+        _checkRecursiveDefinition(t, new HashMap<>(), h);
+    }
+    static final private Object checking = new Object();
+    static final private Object checked = new Object();
+    private void _checkRecursiveDefinition(Type t, Map<Type, Object> marks,
+                                           ExceptionHandler h) {
+        if (marks.get(t)==checking) {
+            h.error("recursive type definition: " + t.toString());
+        }
+        else if (marks.get(t)!=checked) {
+            marks.put(t, checking);
+            if (t instanceof ClassType) {
+                for (var slot: ((ClassType) t).getMembers()) {
+                    Type slotType = typeTable.get(slot.getSpecifier().getTypename());
+                    _checkRecursiveDefinition(slotType, marks, h);
+                }
+            }
+            else if (t instanceof ArrayType) {
+                _checkRecursiveDefinition(((ArrayType) t).getBaseType(), marks, h);
+            }
+            marks.put(t, checked);
+        }
+    }
+}
