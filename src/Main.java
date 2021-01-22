8b64734907d0a3b82e0d30ec00d941661f3a24e6
Heydrich_B <56252029+HeydrichBeillschmidt@users.noreply.github.com>
2021-01-22 13:31:13 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -24,7 +24,8 @@ public class Main {
         InputStream inputStream;
         CharStream input;
         try {
-            inputStream = new FileInputStream(filename);
+//            inputStream = new FileInputStream(filename);
+            inputStream = System.in;
             input = CharStreams.fromStream(inputStream);
         }
         catch (Exception e) {
