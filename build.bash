861b8137dee7abe785c59a41b387a125b55d2fa6
Heydrich_B <56252029+HeydrichBeillschmidt@users.noreply.github.com>
2021-01-22 15:01:15 +0800

    miserable debugging

--- build.bash
+++ build.bash
@@ -1,4 +1,5 @@
 set ff=UNIX
 set -e
+cd "$(dirname "$0")"
 mkdir -p bin
-find ./src -name *.java | javac -d bin -cp /ulib/java/antlr-4.9.1-complete.jar @/dev/stdin
+find ./src -name *.java | javac -d bin -classpath "lib/antlr-4.9.1-complete.jar" @/dev/stdin
