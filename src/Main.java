1f89987b1c6386010226ea1de4142804d11b2a76
Heydrich_B <56252029+HeydrichBeillschmidt@users.noreply.github.com>
2021-01-22 12:36:47 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -20,7 +20,7 @@ public class Main {
     public static void main(String[] args) {
         ExceptionHandler exceptionHandler = new ExceptionHandler();
 
-        String filename = "code.txt";
+        String filename = "test.mx";
         InputStream inputStream;
         CharStream input;
         try {
