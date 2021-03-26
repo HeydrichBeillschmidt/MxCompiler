22c69556a58e9ea9ec129d910719ec0940f18647
HeydrichB <Richard_K@sjtu.edu.cn>
2021-03-26 18:52:41 +0800

    function call matching logic reinforced III

--- src/main/java/Mx/Main.java
+++ src/main/java/Mx/Main.java
@@ -20,7 +20,7 @@ public class Main {
     public static void main(String[] args) {
         ExceptionHandler exceptionHandler = new ExceptionHandler();
 
-        //String filename = "testcases/sema/misc-package/misc-1.mx";
+        //String filename = "testcases/sema/class-package/class-1.mx";
         String filename = "test.mx";
         InputStream inputStream;
         CharStream input;
