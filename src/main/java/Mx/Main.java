282d00d7ce7cf70ede516db2345bc0f99b11524f
HeydrichB <Richard_K@sjtu.edu.cn>
2021-03-26 16:50:13 +0800

    for oj test

--- src/main/java/Mx/Main.java
+++ src/main/java/Mx/Main.java
@@ -20,7 +20,8 @@ public class Main {
     public static void main(String[] args) {
         ExceptionHandler exceptionHandler = new ExceptionHandler();
 
-        String filename = "testcases/sema/misc-package/misc-1.mx";
+        //String filename = "testcases/sema/misc-package/misc-1.mx";
+        String filename = "test.mx";
         InputStream inputStream;
         CharStream input;
         try {
@@ -70,7 +71,7 @@ public class Main {
             exceptionHandler.print();
             throw new RuntimeException();
         }
-
+/*
         IRBuilder irBuilder = new IRBuilder(semanticChecker.getGlobalScope(),
                 semanticChecker.getTypeTable(), exceptionHandler);
         try {
@@ -87,6 +88,6 @@ public class Main {
             throw new RuntimeException();
         }
 
-        new IRPrinter("test.ll").exec(irBuilder.getModule());
+        new IRPrinter("test.ll").exec(irBuilder.getModule());*/
     }
 }
